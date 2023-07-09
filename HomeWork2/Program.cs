36c0b583ccfc1eb1b413e2071f0b2f9e7c5d2858
ionesusanin <ionesusanin@yandex.ru>
2023-07-09 23:28:34 +0300

    Вторая задача

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -10,3 +10,28 @@
 // int result = (randomNumber % 100) / 10;
 
 // Console.WriteLine($"в числе {randomNumber} -> {result}");
+
+
+
+// Напишите программу, которая выводит третью цифру заданного числа 
+ // или сообщает, что третьей цифры нет. До 10 символов
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Length = number.ToString().Length;
+if (Length >= 3)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine($"Третья цифра это {result}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}

18e3b10d1cd32f6cb5dcc7f4ceb487bca8b9a840
ionesusanin <ionesusanin@yandex.ru>
2023-07-21 15:38:27 +0300

    Вторая задача

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -2,15 +2,44 @@
 // два числа (A и B) и возводит число A в натуральную
 // степень B.
 
-Console.WriteLine("Введите число А: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-int numberC = 1;
+// Console.WriteLine("Введите число А: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// int numberC = 1;
 
-for (int i = 1; i<= numberB; i++)
+// for (int i = 1; i<= numberB; i++)
+// {
+//     numberC = numberC * numberA;
+
+// }
+// Console.WriteLine($"Число А в степени В равно { numberC}");
+
+// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите чичло: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int len = Length(a);
+int Length(int a)
 {
-    numberC = numberC * numberA;
-
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
 }
-Console.WriteLine($"Число А в степени В равно { numberC}");
+int sum = 0;
+int Sum(int a, int len)
+{
+    for (int i = 1; i <= len; i++)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
+Console.Write($"Сумма цифр в числе {a} равна: {Sum(a,len)}");

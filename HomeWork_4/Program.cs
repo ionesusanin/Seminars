2187785ce106a386f5f1d562afd1f75b3f510269
ionesusanin <ionesusanin@yandex.ru>
2023-07-22 09:51:49 +0300

    третья задача

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -18,28 +18,49 @@
 // Напишите программу, которая принимает на
 // вход число и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Введите чичло: ");
+// Console.WriteLine("Введите чичло: ");
 
-int a = Convert.ToInt32(Console.ReadLine());
-int len = Length(a);
-int Length(int a)
+// int a = Convert.ToInt32(Console.ReadLine());
+// int len = Length(a);
+// int Length(int a)
+// {
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+// int sum = 0;
+// int Sum(int a, int len)
+// {
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += a % 10;
+//         a /= 10;
+//     }
+//     return sum;
+// }
+// Console.Write($"Сумма цифр в числе {a} равна: {Sum(a,len)}");
+
+
+
+// Напишите программу, которая задаёт массив из 8 элементов 
+// в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] randomArray = new int[8]; 
+
+for (int i = 1; i < 8; i++)
 {
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
+    
+    randomArray[i] = new Random().Next(10,1000);
+    
+
 }
-int sum = 0;
-int Sum(int a, int len)
-{
-    for (int i = 1; i <= len; i++)
-    {
-        sum += a % 10;
-        a /= 10;
-    }
-    return sum;
-}
-Console.Write($"Сумма цифр в числе {a} равна: {Sum(a,len)}");
+ 
+ Console.WriteLine($"Массив: [ {string.Join("; ", randomArray)} ]");
+

f2fdb7ea97bae498f731aa0693a8b1528ebb83da
ionesusanin <ionesusanin@yandex.ru>
2023-07-22 12:12:46 +0300

    третья задача

--- Seminar_5/Program.cs
+++ Seminar_5/Program.cs
@@ -66,3 +66,43 @@
 // }
 // Console.WriteLine($"Массив с перевернутыми элементами: {string.Join(";", reverseArray(array) )}");
 
+
+// Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] getArray(int size, int min, int max)
+{
+    int[] result = new int[size]; 
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max +1);
+    }
+    return result; 
+}
+int[] array = getArray(7, 0 ,10); 
+Console.WriteLine($"Массив: [ {string.Join("; ", array)}] ");
+
+// True - число есть, false - числа нет
+bool findElement(int[] arr, int element)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == element);
+        {
+            return true; // элеменд найден
+        }        
+    }
+    return false; // элемент не найден
+}
+int numberForSearch = 10;
+if (findElement(array, numberForSearch))
+{
+    Console.WriteLine($"Элемент {numberForSearch} присутствует в массиве");
+}
+else
+{
+    Console.WriteLine($"Элемент {numberForSearch} отсутствует в массиве");
+}

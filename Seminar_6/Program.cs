bc0a8953baa5945362f37c90833247069f87b5a7
ionesusanin <ionesusanin@yandex.ru>
2023-07-24 21:23:42 +0300

    третья задача

--- Seminar_6/Program.cs
+++ Seminar_6/Program.cs
@@ -80,15 +80,15 @@
 
 
 
-int a = 10;
-int b = 4;
-int c = 5;
+// int a = 10;
+// int b = 4;
+// int c = 5;
 
-bool IsTriangle(int a, int b, int c)
-{
-   return ( ((a + b) > c) && ((b + c) > a) && ((a + c) > b));
-}
-Console.WriteLine(IsTriangle(a,b,c));
+// bool IsTriangle(int a, int b, int c)
+// {
+//    return ( ((a + b) > c) && ((b + c) > a) && ((a + c) > b));
+// }
+// Console.WriteLine(IsTriangle(a,b,c));
 
 // if (IsTriangle(a ,b ,c))
 // {
@@ -98,3 +98,27 @@ Console.WriteLine(IsTriangle(a,b,c));
 // {
 //     Console.WriteLine($"Треугольник Не существует");
 // }
+
+
+// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 2)
+{
+    return;
+}
+int[] fibonachi = new int[N];
+
+fibonachi[0] = 0;
+fibonachi[1] = 1;
+
+for (int i = 2; i < N; i++)
+{
+    fibonachi[i] = fibonachi[i - 1] + fibonachi[i - 2];
+}
+Console.WriteLine($"Массив: {string.Join("; ", fibonachi)}");

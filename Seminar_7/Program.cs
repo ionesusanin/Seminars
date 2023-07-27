f4f09a082b7c9af8acd2eb82067917d576241d40
ionesusanin <ionesusanin@yandex.ru>
2023-07-27 21:24:43 +0300

    вторая задача

--- Seminar_7/Program.cs
+++ Seminar_7/Program.cs
@@ -57,4 +57,48 @@
 // }
 
 
+// 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
 
+
+Console.Write(" Введите воличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите воличество столбцов: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+// int minRandom = 0;
+// int maxRandom = 10;
+
+int[,] array2D = GetMatrix(rows, colums);
+//Создается таблица из ows строчкек и colums столбцов
+// int[x] - массив одномерный; int [x,y] - двумерный массив; int[x,y,z] -трехмерный
+//Матрица - прямок=уголная таблица размером m (сторочки) на n (столбики)
+PrintMayrix (array2D);
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+
+}
+void PrintMayrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}

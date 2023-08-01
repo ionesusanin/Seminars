c3c7ca8e9161e051f7a44c7381a2b8276fbc7ba5
ionesusanin <ionesusanin@yandex.ru>
2023-08-01 22:12:28 +0300

    вторая задача

--- HomeWork_9/Program.cs
+++ HomeWork_9/Program.cs
@@ -3,15 +3,37 @@
 //  Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-Console.WriteLine("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
 
-string PrintNumbers(int start, int end)
-{
-    if (end == start) return start.ToString();
-    return start +  ", " + PrintNumbers(start -1, end);
+// Console.Write("Введите число M: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int M, int N)
+// {
+//     if (M == N) return M.ToString();
+//     return M +  ", " + PrintNumbers(M + 1, N);
     
+// }
+// Console.WriteLine($"Числа от {M} до {N}: {PrintNumbers(M, N)}");
+
+
+// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+int SumNumbers(int M, int n)
+{
+    if (M == N) return N;
+    return M + SumNumbers(M + 1, N);
+
 }
-Console.WriteLine($"Числа от {n} до 1: {PrintNumbers(n, 1)}");
-
-
+Console.WriteLine($"Сумма элементов от {M} до {N} равно {SumNumbers(M, N)}");
